9a09ffa5775d57075fe7028b76192987a7c4f18b
Thommynator <Thommynator@users.noreply.github.com>
2021-05-04 20:51:42 +0200

    Adds help window (draft)

---
+++ Assets/Scripts/Help.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    public GameObject helpWindow;
+
+    private static bool isOpen = false;
+
+
+    public void ShowHelp()
+    {
+        if (isOpen)
+        {
+            return;
+        }
+
+        helpWindow.SetActive(true);
+        helpWindow.transform.localScale = new Vector3(1, 0, 1);
+        LeanTween.scaleY(helpWindow, 1.0f, 0.25f)
+            .setEaseOutCirc()
+            .setIgnoreTimeScale(true);
+        isOpen = true;
+    }
+
+    public void HideHelp()
+    {
+        if (!isOpen)
+        {
+            return;
+        }
+
+        LeanTween.scaleY(helpWindow, 0.0f, 0.25f)
+            .setEaseOutCirc()
+            .setIgnoreTimeScale(true)
+            .setOnComplete(() => helpWindow.SetActive(false));
+        isOpen = false;
+    }
+}
